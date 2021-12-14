6daedd381548fc7851657ff61fd207c2c5f48b34
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-12-14 15:15:59 +1000

    Create AuthApi

---
+++ frontend/src/api/AuthApi.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+import { config } from '../config';
+import { IUserObject } from '../dto/users/IUserObject';
+
+export const register = async (params: {
+	email: string;
+	username: string;
+	password: string;
+}): Promise<IUserObject> => {
+	const { email, username, password } = params;
+
+	const response = await axios.post<IUserObject>(
+		`${config.apiEndpoint}/auth/register`,
+		{
+			email,
+			username,
+			password,
+		},
+	);
+
+	return response.data;
+};
+
+export const login = async (params: {
+	email: string;
+	password: string;
+}): Promise<IUserObject> => {
+	const { email, password } = params;
+
+	const response = await axios.post<IUserObject>(
+		`${config.apiEndpoint}/auth/login`,
+		{ email, password },
+	);
+
+	return response.data;
+};
