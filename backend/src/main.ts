c0df2a482c6a0142103817eea559d452b1197a73
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-12-17 15:29:16 +1000

    Install helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+
+	app.use(helmet());
 
 	app.useGlobalPipes(new ValidationPipe());
 
