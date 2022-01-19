8e10f3901f6fff4bdaaa5a17300ca9f42fc0d5de
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-01-19 11:48:52 +1000

    Use App Bar with responsive menu

--- frontend/src/AppHeader.tsx
+++ frontend/src/AppHeader.tsx
@@ -1,12 +1,17 @@
+import MenuIcon from '@mui/icons-material/Menu';
 import {
 	AppBar,
 	Box,
 	Button,
 	Container,
+	IconButton,
+	Menu,
+	MenuItem,
 	Stack,
 	Tab,
 	Tabs,
 	Toolbar,
+	Typography,
 } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -24,35 +29,92 @@ const AppHeader = (): React.ReactElement => {
 
 	const { pathname } = useLocation();
 
+	const pages = React.useMemo((): { title: string; path: string }[] => {
+		return [
+			// TODO: { title: t('shared.words'), path: '/translations' },
+			{ title: t('shared.quotes'), path: '/quotes' },
+			{ title: t('shared.artists'), path: '/artists' },
+			{ title: t('shared.users'), path: '/users' },
+		];
+	}, [t]);
+
+	const [anchorElNav, setAnchorElNav] = React.useState<
+		(EventTarget & HTMLButtonElement) | undefined
+	>();
+
+	const handleOpenNavMenu = (
+		e: React.MouseEvent<HTMLButtonElement>,
+	): void => {
+		setAnchorElNav(e.currentTarget);
+	};
+
+	const handleCloseNavMenu = (): void => {
+		setAnchorElNav(undefined);
+	};
+
 	return (
 		<AppBar position="sticky">
 			<Container maxWidth="lg">
 				<Toolbar variant="dense" disableGutters>
-					<Tabs value={`/${pathname.split('/')[1]}`}>
-						{/*<Tab
-							label={t('shared.words')}
-							value="/translations"
-							component={RouterLink}
-							to="/translations"
-						/>*/}
-						<Tab
-							label={t('shared.quotes')}
-							value="/quotes"
-							component={RouterLink}
-							to="/quotes"
-						/>
-						<Tab
-							label={t('shared.artists')}
-							value="/artists"
-							component={RouterLink}
-							to="/artists"
-						/>
-						<Tab
-							label={t('shared.users')}
-							value="/users"
-							component={RouterLink}
-							to="/users"
-						/>
+					<Box
+						sx={{
+							flexGrow: 1,
+							display: { xs: 'flex', md: 'none' },
+						}}
+					>
+						<IconButton
+							size="large"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: 'block', md: 'none' },
+							}}
+						>
+							{pages.map((page) => (
+								<MenuItem
+									onClick={handleCloseNavMenu}
+									component={RouterLink}
+									to={page.path}
+									key={page.path}
+								>
+									<Typography textAlign="center">
+										{page.title}
+									</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+
+					<Tabs
+						value={`/${pathname.split('/')[1]}`}
+						sx={{ display: { xs: 'none', md: 'flex' } }}
+					>
+						{pages.map((page) => (
+							<Tab
+								label={page.title}
+								value={page.path}
+								component={RouterLink}
+								to={page.path}
+								key={page.path}
+							/>
+						))}
 					</Tabs>
 					<Box sx={{ flexGrow: 1 }} />
 					<Stack direction="row" spacing={2}>
