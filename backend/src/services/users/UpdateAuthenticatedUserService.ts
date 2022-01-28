b31355accb806860eb2cd3ec948a7544ea27042d
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-01-28 17:12:38 +1000

    Validate

--- backend/src/services/users/UpdateAuthenticatedUserService.ts
+++ backend/src/services/users/UpdateAuthenticatedUserService.ts
@@ -1,6 +1,7 @@
 import { EntityManager, EntityRepository } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { BadRequestException, Injectable } from '@nestjs/common';
+import Joi, { ObjectSchema } from 'joi';
 
 import { AuthenticatedUserObject } from '../../dto/users/AuthenticatedUserObject';
 import { User } from '../../entities/User';
@@ -26,10 +27,24 @@ export class UpdateAuthenticatedUserService {
 	): Promise<AuthenticatedUserObject> {
 		const { password, username, email, newPassword } = params;
 
+		const schema: ObjectSchema<{
+			username: string;
+			email: string;
+			password: string;
+		}> = Joi.object({
+			password: Joi.string().required(),
+			username: Joi.string().optional().trim().min(2).max(32),
+			email: Joi.string().optional().email().max(50),
+			newPassword: Joi.string().optional().min(8),
+		});
+
+		const result = schema.validate(params, { convert: true });
+
+		if (result.error)
+			throw new BadRequestException(result.error.details[0].message);
+
 		return this.em.transactional(async () => {
 			if (!this.permissionContext.user) throw new BadRequestException();
-
-			// TODO: Validate.
 
 			const user = await this.userRepo.findOneOrFail({
 				id: this.permissionContext.user.id,
@@ -55,14 +70,10 @@ export class UpdateAuthenticatedUserService {
 			// TODO: Log.
 
 			if (username) {
-				// TODO: Validate.
-
 				user.name = username;
 			}
 
 			if (email) {
-				// TODO: Validate.
-
 				const normalizedEmail =
 					await this.normalizeEmailService.normalizeEmail(email);
 
@@ -80,8 +91,6 @@ export class UpdateAuthenticatedUserService {
 			}
 
 			if (newPassword) {
-				// TODO: Validate.
-
 				await user.updatePassword(
 					this.passwordHasherFactory.default,
 					newPassword,
