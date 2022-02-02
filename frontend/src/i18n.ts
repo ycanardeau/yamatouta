0d623339a00c108e4799c78a92f1847b1732e0aa
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-02-02 10:00:48 +1000

    Enable browser caching with local storage

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
+import ChainedBackend from 'i18next-chained-backend';
+import HttpBackend from 'i18next-http-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(Backend)
+i18n.use(ChainedBackend)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
@@ -13,5 +15,14 @@ i18n.use(Backend)
 		},
 		react: {
 			useSuspense: false,
+		},
+		backend: {
+			backends: [LocalStorageBackend, HttpBackend],
+			backendOptions: [
+				{
+					expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+				},
+				{},
+			],
 		},
 	});
