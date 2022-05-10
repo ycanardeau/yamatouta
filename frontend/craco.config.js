022a9d351a907e528e506946c6cf68fd3e7065ce
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-05-10 23:36:08 +1000

    Install webpack-retry-chunk-load-plugin

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,1 +1,14 @@
-module.exports = {};
+const { RetryChunkLoadPlugin } = require('webpack-retry-chunk-load-plugin');
+
+module.exports = {
+	webpack: {
+		plugins: [
+			new RetryChunkLoadPlugin({
+				cacheBust: `function() {
+					return Date.now()
+				}`,
+				maxRetries: 5,
+			}),
+		],
+	},
+};
