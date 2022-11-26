6144bb4848d218e2f90c48ccce9983410f921b34
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-26 08:18:05 +0000

    Move code from index.tsx to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,57 @@
 import AppRoutes from '@/AppRoutes';
+import { AuthProvider } from '@/AuthProvider';
 import Header from '@/Header';
 import SideNav from '@/SideNav';
+import config from '@/config';
+import '@/i18n';
 import { EuiPage, EuiPageBody, EuiPageSideBar } from '@elastic/eui';
+import { EuiProvider } from '@elastic/eui';
+import '@elastic/eui/dist/eui_theme_dark.css';
+import createCache from '@emotion/cache';
 import { ScrollToTop } from '@vocadb/route-sphere';
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import './App.scss';
 
+axios.defaults.baseURL = config.apiEndpoint;
+axios.defaults.withCredentials = true;
+
+axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
+
+// See https://github.com/elastic/eui/issues/5436 and https://elastic.github.io/eui/#/utilities/provider.
+const emotionCache = createCache({
+	key: 'app',
+	container:
+		document.querySelector<HTMLElement>('meta[name="emotion-global"]') ??
+		undefined,
+});
+
 const App = (): React.ReactElement => {
 	return (
-		<>
-			<ScrollToTop />
+		<AuthProvider>
+			<BrowserRouter>
+				<EuiProvider colorMode="dark" cache={emotionCache}>
+					<ScrollToTop />
 
-			<Header />
+					<Header />
 
-			<EuiPage paddingSize="none">
-				<EuiPageSideBar paddingSize="l" sticky>
-					<SideNav />
-				</EuiPageSideBar>
+					<EuiPage paddingSize="none">
+						<EuiPageSideBar paddingSize="l" sticky>
+							<SideNav />
+						</EuiPageSideBar>
 
-				<EuiPageBody panelled>
-					<React.Suspense fallback={null /* TODO */}>
-						<AppRoutes />
-					</React.Suspense>
-				</EuiPageBody>
-			</EuiPage>
-		</>
+						<EuiPageBody panelled>
+							<React.Suspense fallback={null /* TODO */}>
+								<AppRoutes />
+							</React.Suspense>
+						</EuiPageBody>
+					</EuiPage>
+				</EuiProvider>
+			</BrowserRouter>
+		</AuthProvider>
 	);
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,42 +1,14 @@
 import App from '@/App';
-import { AuthProvider } from '@/AuthProvider';
-import config from '@/config';
-import '@/i18n';
 import reportWebVitals from '@/reportWebVitals';
-import { EuiProvider } from '@elastic/eui';
-import '@elastic/eui/dist/eui_theme_dark.css';
-import createCache from '@emotion/cache';
-import axios from 'axios';
-import axiosRetry from 'axios-retry';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
-axios.defaults.baseURL = config.apiEndpoint;
-axios.defaults.withCredentials = true;
-
-axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
-
-// See https://github.com/elastic/eui/issues/5436 and https://elastic.github.io/eui/#/utilities/provider.
-const emotionCache = createCache({
-	key: 'app',
-	container:
-		document.querySelector<HTMLElement>('meta[name="emotion-global"]') ??
-		undefined,
-});
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 root.render(
 	<React.StrictMode>
-		<AuthProvider>
-			<BrowserRouter>
-				<EuiProvider colorMode="dark" cache={emotionCache}>
-					<App />
-				</EuiProvider>
-			</BrowserRouter>
-		</AuthProvider>
+		<App />
 	</React.StrictMode>,
 );
 
